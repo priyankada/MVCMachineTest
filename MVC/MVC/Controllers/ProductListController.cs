30963c0799ead639d5a36becc28d6a067719cb14
unknown <datkhilepriyanka@gmail.com>
2020-11-03 17:02:18 +0530

    first commit

---
+++ MVC/MVC/Controllers/ProductListController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class ProductListController : Controller
+    {
+        CatProEntities1 db = new CatProEntities1();
+        //
+        // GET: /ProductList/
+
+        public ActionResult Index()
+        {
+            var query = from p in db.Products
+                        join c in db.Categories on p.CatId equals c.CategoryId
+
+                        select new ProductWithCategory { Product = p, Category = c };
+            var model = query.ToList();
+            return View(model);
+        }
+
+    }
+}
---
+++ MVC/MVC/Models/ProductWithCategory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class ProductWithCategory
+    {
+        public ProductWithCategory()
+        {
+
+        }
+        public ProductWithCategory(Product p, Category c)
+        {
+            //TO DO: Complete Member Initialization  
+            this.Product = p;
+            this.Category = c;
+        }
+        public Product Product { get; set; }
+        public Category Category { get; set; }
+    }
+}
